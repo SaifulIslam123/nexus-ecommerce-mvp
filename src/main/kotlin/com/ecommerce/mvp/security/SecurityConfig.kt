4f14197e0d08fae181e4500364f41c1b61da5960
Mohammad Saiful Islam <saifulislamtfp@gmail.com>
2026-03-01 02:11:04 +0800

    Remove commented-out authentication manager beans from SecurityConfig

--- src/main/kotlin/com/ecommerce/mvp/security/SecurityConfig.kt
+++ src/main/kotlin/com/ecommerce/mvp/security/SecurityConfig.kt
@@ -2,16 +2,13 @@ package com.ecommerce.mvp.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.Customizer
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
@@ -27,25 +24,6 @@ class SecurityConfig(
         val algorithm = Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA1
         return Pbkdf2PasswordEncoder("secret, encrypt me ;)", 16, 128, algorithm)
     }
-
-    /*@Bean
-    fun authenticationManager(
-        http: HttpSecurity,
-        passwordEncoder: Pbkdf2PasswordEncoder
-    ): AuthenticationManager {
-        val authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder::class.java)
-        authenticationManagerBuilder
-            .userDetailsService(customerUserDetailsService)
-            .passwordEncoder(passwordEncoder)
-        authenticationManagerBuilder.parentAuthenticationManager(null)
-        return authenticationManagerBuilder.build()
-    }*/
-
-    /*@Bean
-    @Throws(Exception::class)
-    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager {
-        return config.authenticationManager
-    }*/
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
