245f286692e62ba1fb3962bcdd80c5acb2ebd5dc
Mohammad Saiful Islam <saifulislamtfp@gmail.com>
2026-03-01 02:09:31 +0800

    Add user registration endpoint to AuthController

--- src/main/kotlin/com/ecommerce/mvp/modules/auth/controller/AuthController.kt
+++ src/main/kotlin/com/ecommerce/mvp/modules/auth/controller/AuthController.kt
@@ -1,16 +1,23 @@
 package com.ecommerce.mvp.modules.auth.controller
 
+import com.ecommerce.mvp.modules.user.model.dto.UserDto
+import com.ecommerce.mvp.modules.user.service.UserService
 import com.ecommerce.mvp.security.JwtUtil
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api/auth")
 class AuthController(
+    private val userService: UserService,
     private val authenticationManager: AuthenticationManager,
-    private val jwtUtil: JwtUtil
+    private val jwtUtil: JwtUtil,
+    private val passwordEncoder: BCryptPasswordEncoder
 ) {
 
     @PostMapping("/login")
@@ -24,6 +31,16 @@ class AuthController(
         val token = jwtUtil.generateToken(request.username)
         return ResponseEntity.ok(token)
     }
+
+    @PostMapping("/register")
+    fun register(@Valid @RequestBody request: UserDto): ResponseEntity<UserDto> {
+
+        val userDto = userService.registerUser(request.apply {
+            password = passwordEncoder.encode(password)
+        })
+        return ResponseEntity.status(HttpStatus.CREATED).body(userDto)
+    }
+
 }
 
 // Idiomatic Kotlin Data Class (includes Getters/Setters/Constructor)
