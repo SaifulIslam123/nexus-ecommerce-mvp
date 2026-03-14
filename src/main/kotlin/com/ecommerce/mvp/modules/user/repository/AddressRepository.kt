510fbe0fe2ddb8e20a972068864e0a7558715f82
Mohammad Saiful Islam <saifulislamtfp@gmail.com>
2026-03-15 04:01:07 +0800

    Address entity Soft delete logic implement

--- src/main/kotlin/com/ecommerce/mvp/modules/user/repository/AddressRepository.kt
+++ src/main/kotlin/com/ecommerce/mvp/modules/user/repository/AddressRepository.kt
@@ -3,9 +3,10 @@ package com.ecommerce.mvp.modules.user.repository
 import com.ecommerce.mvp.modules.user.model.entity.Address
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 interface AddressRepository : JpaRepository<Address, Long> {
-    fun findByUserId(userId: Long): List<Address>
+    fun findByIdAndUserIdAndDeletedAtIsNull(id: Long, userId: Long): Optional<Address>
 }
 
