cff2bced04256868893c6482c1b88661c28d55ff
Md. Saiful Islam <saiful.islam@carsome.com>
2026-03-28 22:58:21 +0800

    Add functionality to add items to the cart with validation in CartService and CartController

--- src/main/kotlin/com/ecommerce/mvp/modules/cart/CartController.kt
+++ src/main/kotlin/com/ecommerce/mvp/modules/cart/CartController.kt
@@ -1,9 +1,13 @@
 package com.ecommerce.mvp.modules.cart
 
 import com.ecommerce.mvp.common.response.ApiResponse
+import com.ecommerce.mvp.modules.cart.model.dto.CartItemRequestDto
 import com.ecommerce.mvp.modules.cart.model.dto.CartResponseDto
 import com.ecommerce.mvp.modules.cart.service.CartService
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -22,6 +26,11 @@ class CartController(
     @GetMapping
     fun getCart(): CartResponseDto {
         return cartService.getCart()
+    }
+
+    @PostMapping
+    fun addCart(@Valid @RequestBody cartItemRequestDto: CartItemRequestDto) {
+        cartService.addItemToCart(cartItemRequestDto)
     }
 }
 
