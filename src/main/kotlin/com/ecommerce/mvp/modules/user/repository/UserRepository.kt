bd97470d3fc8c92282e7c5a66739f025a1b26f28
Md. Saiful Islam <saiful.islam@carsome.com>
2026-04-07 03:32:37 +0800

    Refactor UserController, UserService, and UserDto to improve address handling and streamline user response DTOs

--- src/main/kotlin/com/ecommerce/mvp/modules/user/repository/UserRepository.kt
+++ src/main/kotlin/com/ecommerce/mvp/modules/user/repository/UserRepository.kt
@@ -1,5 +1,6 @@
 package com.ecommerce.mvp.modules.user.repository
 
+import com.ecommerce.mvp.modules.user.model.entity.Address
 import com.ecommerce.mvp.modules.user.model.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
@@ -11,5 +12,9 @@ interface UserRepository : JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     fun findByUserEmail(email: String?): User?
 
+    @Query("SELECT user FROM User user JOIN FETCH user.addresses address WHERE user.email = :email AND address.id = :addressId")
+    fun findByUserEmailAndAddressId(email: String?, addressId: Long): User?
+
+    //fun findByIdAndUserIdAndDeletedAtIsNull(id: Long, userId: Long): Address?
 
 }
