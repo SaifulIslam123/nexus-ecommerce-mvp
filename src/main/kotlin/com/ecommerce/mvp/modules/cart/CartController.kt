ba8858a9a0af9185e70407db267bb4c3112ed39a
Mohammad Saiful Islam <saifulislamtfp@gmail.com>
2026-04-07 12:34:48 +0800

    Refactor CartController to update API endpoint for cart management

--- src/main/kotlin/com/ecommerce/mvp/modules/cart/CartController.kt
+++ src/main/kotlin/com/ecommerce/mvp/modules/cart/CartController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/cart")
+@RequestMapping("/api/users/cart")
 class CartController(
     private val cartService: CartService
 ) {
