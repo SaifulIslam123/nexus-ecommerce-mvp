80d94da7c74ce8e268e4528c2d3d48aa9fa817ec
Saiful <saifulislamtfp@gmail.com>
2026-08-22 22:50:07 +0800

    Update AuditorAwareImpl to retrieve current user from SecurityContext

--- src/main/kotlin/com/ecommerce/mvp/common/entity/audit/AuditorAwareImpl.kt
+++ src/main/kotlin/com/ecommerce/mvp/common/entity/audit/AuditorAwareImpl.kt
@@ -1,6 +1,7 @@
 package com.ecommerce.mvp.common.entity.audit
 
 import org.springframework.data.domain.AuditorAware
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import java.util.Optional
 
@@ -9,6 +10,6 @@ class AuditorAwareImpl : AuditorAware<String> {
     override fun getCurrentAuditor(): Optional<String> {
         // In a real application, you would get the current user from the SecurityContext
         // For this example, we return a hardcoded value:
-        return Optional.of("Saiful")
+        return Optional.of(SecurityContextHolder.getContext().authentication?.name ?: "UNKNOWN")
     }
 }
