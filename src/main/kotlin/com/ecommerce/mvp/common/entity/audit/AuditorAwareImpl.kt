3fa116cce6da8cdc46e34efc7ccc27f92d99b1d6
Saiful <saifulislamtfp@gmail.com>
2026-08-30 00:38:40 +0800

    Remove hardcoded user value from AuditorAwareImpl and retrieve current user from SecurityContext

--- src/main/kotlin/com/ecommerce/mvp/common/entity/audit/AuditorAwareImpl.kt
+++ src/main/kotlin/com/ecommerce/mvp/common/entity/audit/AuditorAwareImpl.kt
@@ -8,8 +8,6 @@ import java.util.Optional
 @Component
 class AuditorAwareImpl : AuditorAware<String> {
     override fun getCurrentAuditor(): Optional<String> {
-        // In a real application, you would get the current user from the SecurityContext
-        // For this example, we return a hardcoded value:
         return Optional.of(SecurityContextHolder.getContext().authentication?.name ?: "UNKNOWN")
     }
 }
